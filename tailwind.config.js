e5d659b001a823866e3016322a2e7368bc63277e
Hanglesson Fontines <72349939+Hanglesson@users.noreply.github.com>
2021-12-28 17:07:35 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        revolver: {
+          0: "#fcf2ff",
+          5: "#f8e1ff",
+          10: "#f3d1fe",
+          20: "#e5aff9",
+          30: "#d68fef",
+          40: "#c374e0",
+          50: "#ae5ccb",
+          60: "#9447af",
+          70: "#75348b",
+          80: "#522261",
+          90: "#2d1235",
+        },
+        mercury: {
+          0: "#ffffff",
+          5: "#fdfdfd",
+          10: "#fbfbfb",
+          20: "#f7f6f6",
+          30: "#f1efef",
+          40: "#e9e6e6",
+          50: "#e0dbdb",
+          60: "#d4cece",
+          70: "#c4bebe",
+          80: "#b1abab",
+          90: "#9e9898",
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      container: {
+        center: true,
+      },
+    },
+  },
+  plugins: [],
+};
