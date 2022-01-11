d8bf2e282cd17ee0b74d86dc9630c2d7e8029b6f
Hanglesson Fontines <72349939+Hanglesson@users.noreply.github.com>
2022-01-11 09:51:20 -0500

    :up: Alpine js integrations tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 // import "./styles.css";
+
+let dummyTransactions = [
+  {
+    id: 01,
+    descricao: "Suite 01",
+    valor: 53,
+    tipo: "entrada",
+    status: "completo",
+  },
+  {
+    id: 02,
+    descricao: "Suite 02",
+    valor: 47,
+    tipo: "entrada",
+    status: "pendente",
+  },
+  {
+    id: 03,
+    descricao: "Cartão",
+    valor: 47,
+    tipo: "cartao",
+    status: "",
+  },
+];
+
+function App() {
+  this.transacoes = dummyTransactions;
+  return {
+    msgTeste: "Teste OK!",
+    paraMoeda: new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }),
+    transacoes,
+    entradas() {
+      return this.transacoes.filter((transacao) => transacao.tipo == "entrada");
+    },
+    atualizaBalanco: (arr) => {
+      const montante = arr.map((transacao) => transacao.valor);
+      const total = montante.reduce(
+        (acumulador, valor) => acumulador + valor,
+        0
+      );
+      return total;
+    },
+    addTransacao(desc, montante, tipo) {
+      this.transacoes.push({
+        id: this.transacoes.length + 1,
+        descricao: desc,
+        valor: Number(montante),
+        tipo: tipo,
+        status: "completo",
+      });
+    },
+  };
+}
